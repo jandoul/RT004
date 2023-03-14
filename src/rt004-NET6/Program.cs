2b9b3b952d5886256397af698330ba9f71d55ca7
Lukáš Jandourek <38855424+jandoul@users.noreply.github.com>
2023-03-14 12:10:33 +0100

    Implemented HDR image drawing.
    Also added a new parameter and a more detailed error message.

--- src/rt004-NET6/Program.cs
+++ src/rt004-NET6/Program.cs
@@ -5,29 +5,32 @@ using Util;
 
 namespace rt004;
 
-// Holds information about the generated image
+// Holds information about the generated image and all of the parameters
 class Image
 {
     public int Width { get; set; }
     public int Height { get; set; }
     public string? Filename { get; set; }
+    public int Parameter { get; set; }
 }
 
 
 internal class Program
 {
+    // Loads parameters into the Image from the command-line or config file
     public static bool GetCommandLineArguments(string[] args, out Image image)
     {
         image = new Image();
 
         try
         {
-            if (args.Length == 3) // Command-line arguments
+            if (args.Length == 4) // Command-line arguments
             {
                 Console.WriteLine("Reading parameters from command-line.");
                 image.Width = Int32.Parse(args[0]);
                 image.Height = Int32.Parse(args[1]);
                 image.Filename = args[2];
+                image.Parameter = Int32.Parse(args[3]);
             }
             else if (args.Length == 1) // Config file
             {
@@ -46,6 +49,7 @@ internal class Program
             if (ex is IOException || ex is IndexOutOfRangeException || ex is FormatException)
             {
                 Console.WriteLine("Argument error.");
+                Console.WriteLine("(Correct usage: 'image_width image_height image_path parameter' or 'config_file_path')");
                 return false;
             }
         }
@@ -62,12 +66,21 @@ internal class Program
         if (GetCommandLineArguments(args, out image) == false)
             return;
 
-
         // HDR image.
         FloatImage fi = new FloatImage(image.Width, image.Height, 3);
 
-        // TODO: put anything interesting into the image.
-        // TODO: use fi.PutPixel() function, pixel should be a float[3] array [R, G, B]
+        // Put an interesting pattern into the image
+        for (int i = 0; i < image.Width; i++)
+        {
+            for (int j = 0; j < image.Height; j++)
+            {
+                float R = (float)Math.Cos((float)i / (1+j & (i * j)));
+                float G = (float)Math.Sin((float)j / (1+i & (i * j)));
+                float B = (float)Math.Cos((float)(i+G*100)*(j+R*100) / (i*j));
+                float[] pixel = new float[3] { R, G, B };
+                fi.PutPixel(i, j, pixel);
+            }
+        }
 
         //fi.SaveHDR(fileName);   // Doesn't work well yet...
         if (image.Filename is null)
