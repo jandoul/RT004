abbed4136b0d6014377d5c2e5ecc60e5180cc84b
Lukáš Jandourek <Crazyk852@email.cz>
2023-04-18 11:56:51 +0200

    Added Camera class

---
+++ s04-Camera/Camera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rt004
+{
+    internal class Camera
+    {
+        public Vector3 position;
+
+        // The orientation of the camera (all 3 vectors are orthonormal)
+        public Vector3 view;
+        public Vector3 right;
+        public Vector3 up;
+
+        public double d;        // Distance from projection plane
+
+        public Camera(Vector3 position, Vector3 view, double angle, double d)
+        {
+            this.position = position;
+            this.d = d;
+
+            this.view = view.Normalize();
+            this.up = new Vector3(0, 1.0, 0);
+            this.right = Vector3.CrossProduct(view, up).Normalize();
+            this.up = Vector3.CrossProduct(view, -1.0 * right).Normalize();
+        }
+    }
+}
